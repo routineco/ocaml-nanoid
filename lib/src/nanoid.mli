0d58578b4943782ee9276dabdfd3f04539910e98
mefyl <mefyl@gruntech.org>
2021-09-30 08:42:24 +0200

    Add modules introductory documentation.

--- lib/src/nanoid.mli
+++ lib/src/nanoid.mli
@@ -1,3 +1,23 @@
+(** Implementation of {{:https://github.com/ai/nanoid} Nano ID}, a tiny, secure,
+    URL-friendly, unique string ID generator popular in the javascript
+    ecosystem.
+
+    The interface mimics the Javascript interface as closely as possible with a
+    single {!nanoid} entrypoint. Custom alphabets are not yet supported.
+
+    Both native and js_of_ocaml environments are supported via a
+    {{:https://dune.readthedocs.io/en/stable/variants.html)} virtual library}:
+    [nanoid]. Libraries should depend on this library, and final executables
+    should pick an implementation by depending either on [nanoid.os] or
+    [nanoid.jsoo].
+
+    The native implementation mimics the javascript one and relies by default on
+    [cryptokit] pseudo random number generator seeded with the current unix time
+    as given by [Unix.gettimeofday]. This should yield satisfying results for
+    most application without ever blocking, unless you need strong cryptographic
+    guarantees for security reasons. Use the [Nanoid_os] library to customize
+    the random number generation. *)
+
 (** [nanoid ?size ()] generates a random Nano ID of size [size], which defaults
     to 21. *)
 val nanoid : ?size:int -> unit -> string
--- lib/src/os/nanoid_os.mli
+++ lib/src/os/nanoid_os.mli
@@ -1,3 +1,9 @@
+(** Native, customizable implementation of {{:https://github.com/ai/nanoid} Nano
+    ID}. Unless you specifically want to customize random number generation, you
+    should probably use the simpler [nanoid] library, which uses the [Simple ()]
+    implementation from this library. *)
+
+(** @inline*)
 include module type of Intf
 
 (** [pseudo_seeded seed] is a [RNG] module that generates pseudo random numbers
@@ -8,7 +14,7 @@ val pseudo_seeded : string -> (module RNG)
 (** [Make (Rng)] is a Nano ID generator based on random number generator [Rng] *)
 module Make (Rng : RNG) : S
 
-(** [Simle ()] is a Nano ID generator based on the {!pseudo_seeded} random
+(** [Simple ()] is a Nano ID generator based on the {!pseudo_seeded} random
     number generator seeded with the current system time as given by
     {!Unix.gettimeofday}. *)
 module Simple () : S
